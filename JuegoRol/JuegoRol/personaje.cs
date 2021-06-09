476d51fc5cdf77c9f0f1dfc6993cba2a0d554c10
lucaslunacl <lucaslunaclaraso@gmail.com>
2021-06-09 16:00:58 -0300

    Creando la clase personaje y constructor

--- JuegoRol/JuegoRol/personaje.cs
+++ JuegoRol/JuegoRol/personaje.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace JuegoRol
 {
-    class personaje
+    public enum TipoDePersonaje{
+        Orco,
+        Elfo,
+        Hobbit,
+        Humano,
+    }
+    public class personaje
     {
+        // DATOS
+        private TipoDePersonaje tipo;
+        private string nombre;
+        private string apodo;
+        private DateTime FecNac;
+        private int edad;
+        private int salud;
+
+        // CARACTERISTICAS 
+        private int velocidad;
+        private int destreza;
+        private int fuerza;
+        private int nivel;
+        private int armadura;
+
+
+        // METODO CONSTRUCTOR
+
+        public personaje()
+        {
+            Salud = 100; //asigno a cada personaje creado salud 100 x defecto
+
+        }
+
+        public TipoDePersonaje Tipo { get => tipo; set => tipo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apodo { get => apodo; set => apodo = value; }
+        public DateTime FecNac1 { get => FecNac; set => FecNac = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public int Salud { get => salud; set => salud = value; }
+        public int Velocidad { get => velocidad; set => velocidad = value; }
+        public int Destreza { get => destreza; set => destreza = value; }
+        public int Fuerza { get => fuerza; set => fuerza = value; }
+        public int Nivel { get => nivel; set => nivel = value; }
+        public int Armadura { get => armadura; set => armadura = value; }
     }
 }
